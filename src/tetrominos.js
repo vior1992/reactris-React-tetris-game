e0ce93b82e0aa5d5ed0c6aea71bfa8c52c69bd7d
Dani <vior1992@gmail.com>
2019-08-20 20:02:58 +0300

    add & done tetrominos

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -24,5 +24,42 @@ export const TETROMINOS = {
             [0, 'L', 'L']
         ],
         color: '223, 173, 36'
-    }
+    },
+    O: { 
+        shape: [
+            ['O', 'O'],
+            ['O', 'O']
+        ],
+        color: '223, 217, 36'
+    },
+    S: { 
+        shape: [
+            [0, 'S', 'S'],
+            ['S', 'S', 0],
+            [0, 0, 0]
+        ],
+        color: '48, 211, 56'
+    },
+    T: { 
+        shape: [
+            [0, 0, 0],
+            ['T', 'T', 'T'],
+            [0, 'T', 0]
+        ],
+        color: '132, 61, 198'
+    },
+    Z: { 
+        shape: [
+            ['Z', 'Z', 0],
+            [0, 'Z', 'Z'],
+            [0, 0, 0]
+        ],
+        color: '227, 78, 78'
+    },
+};
+
+export const randomTetromino = () => {
+    const tetrominos = 'IJLOSTZ';
+    const randomTetromino = tetrominos[Math.floor(Math.random() * tetrominos.length)];
+    return TETROMINOS[randomTetromino];
 };
